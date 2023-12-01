a6237291600af7f392a90424179d359b50a54999
rolika0x <105377936+rolika0x@users.noreply.github.com>
2023-12-01 19:41:35 +0100

    aa

--- netlify-c.js
+++ netlify-c.js
@@ -8,13 +8,3 @@ netlifyIdentity.on('login', () => {
     // Redirect after login
     window.location.href = '/galaxylibrary/';
   });
-  
-  
-
-// Bind to events
-netlifyIdentity.on('init', user => console.log('init', user));
-netlifyIdentity.on('login', user => console.log('login', user));
-netlifyIdentity.on('logout', () => console.log('Logged out'));
-netlifyIdentity.on('error', err => console.error('Error', err));
-netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
