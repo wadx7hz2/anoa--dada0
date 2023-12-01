a2f7914c16e88d98576fdbb9e4f5a5baba02167c
rolika0x <105377936+rolika0x@users.noreply.github.com>
2023-12-01 19:51:33 +0100

    m

--- logout.js
+++ logout.js
@@ -3,6 +3,5 @@ if (netlifyIdentity.currentUser() !== null) {
     // User is logged in, perform your actions
     console.log('User is logged in');
   } else {
-    // User is not logged in, redirect to 'x' page
-    window.location.href = 'https://dash-galaxy.netlify.app/'; // Replace '/x-page' with your desired page
+
   }
