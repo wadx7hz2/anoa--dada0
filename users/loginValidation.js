60d6e541972b9c50e74d8ec45d6db9ed987e1501
rolika0x <105377936+rolika0x@users.noreply.github.com>
2023-12-03 19:34:52 +0100

    aa

--- users/loginValidation.js
+++ users/loginValidation.js
@@ -3,8 +3,8 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   
   // Object with multiple usernames and passwords, along with their corresponding directories
   const users = {
-    ju: { password: '960LJ', directory: 'galaxylibrary/' },
-    ap: { password: 'CL5D5', directory: 'galaxylibrary/' },
+    mm: { password: '960LJ', directory: 'galaxylibrary/' },
+    aa: { password: 'CL5D5', directory: 'galaxylibrary/' },
     // Add more users here as needed
   };
   
@@ -15,10 +15,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   // Check if the entered username exists and if the password matches
   if (users[username] && users[username].password === password) {
     const redirectURL = users[username].directory;
-    alert('Login successful!');
     // Redirect the user to their respective directory upon successful login
     window.location.href = redirectURL;
   } else {
-    alert('Invalid username or password. Please try again.');
+    alert('Kérjük próbálkozzon újra.');
   }
 });
